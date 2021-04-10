11d7f269638ba202562d3fe707b5a7055529321c
fengzk-dev <fengzhk@mail2.sysu.com>
2021-04-11 03:28:44 +0800

    add generaor pattern

---
+++ Generator Pattern/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func fib(n int) chan int {
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		for i, j := 0, 1; i < n; i, j = i + j, i {
+			out <- i
+		}
+	}()
+
+	return out
+}
+
+func main() {
+	for x := range fib(1000000) {
+		fmt.Println(x)
+	}
+	
+}
